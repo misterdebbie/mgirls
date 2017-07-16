b9e10d9c5317e25848587a86fd6dbfe069ab2616
mr.debbie <giyoungchong1980@gmail.com>
2017-07-16 14:35:08 -0500

    Fashion Stuff

--- app_server/controllers/sample_2Controllers/test.js
+++ app_server/controllers/sample_2Controllers/test.js
@@ -1,4 +1,9 @@
 /* GET sample 2 home page */
 module.exports.sampleHomeController = function (req, res, next) {
-    res.render('samplenav', { title: 'sample 2 test page' });
+    res.render('samplenav', { title: 'Designers',
+        designers: ["Acne","A.P.C.","Bally","Boglioli",
+        "Canalli","Cerruti","Coach","Dior",
+        "E.Tautz","Etro","E.Zegna","Ferragamo","Gucci",
+        "Hermes","Issey Miyake","J.Crew","J.W.Anderson"]
+    });
 };
