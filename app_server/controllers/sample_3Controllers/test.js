3940f0cfb3fd89e0e116ce37da57d6abb123aa39
mr.debbie <giyoungchong1980@gmail.com>
2017-07-23 13:05:39 -0500

    add lady page to sample 3

--- app_server/controllers/sample_3Controllers/test.js
+++ app_server/controllers/sample_3Controllers/test.js
@@ -1,4 +1,13 @@
 /* GET sample 3 home page */
-module.exports.sampleHomeController = function (req, res, next) {
+/*module.exports.sampleHomeController = function (req, res, next) {
     res.render('home3', { title: 'sample 3 test page'});
+};*/
+
+/* get home page for all djebbie picks */
+module.exports.sampleHomeController = function (req, res, next) {
+    res.render('djebbiepickshome', {});
+};
+/*get home page for lady */
+module.exports.ladyController = function (req, res, next) {
+    res.render('ladypage', {});
 };
--- app_server/routes/three.js
+++ app_server/routes/three.js
@@ -5,6 +5,6 @@ var ctrlS3 = require('../controllers/sample_3Controllers/test');
 /* GET sample 3 home page. */
 /* base URL is '/samplethree' */
 router.get('/', ctrlS3.sampleHomeController);
-
+router.get('/ladypage', ctrlS3.ladyController);
 
 module.exports = router;
