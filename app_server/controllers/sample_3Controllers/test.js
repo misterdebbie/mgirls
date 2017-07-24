f94453e799c49b9e6d7cb1f135626f84d0110402
mr.debbie <giyoungchong1980@gmail.com>
2017-07-23 21:21:44 -0500

    add clouds

--- app_server/controllers/sample_3Controllers/test.js
+++ app_server/controllers/sample_3Controllers/test.js
@@ -4,10 +4,38 @@
 };*/
 
 /* get home page for all djebbie picks */
-module.exports.sampleHomeController = function (req, res, next) {
+/*module.exports.sampleHomeController = function (req, res, next) {
     res.render('djebbiepickshome', {});
+};*/
+/* get home page for all djebbie picks */
+module.exports.sampleHomeController = function (req, res, next) {
+    res.render('dj', {});
 };
 /*get home page for lady */
 module.exports.ladyController = function (req, res, next) {
     res.render('ladypage', {});
+};
+/*get home page for daewoo */
+module.exports.daewooController = function (req, res, next) {
+    res.render('daewoopage', {});
+};
+/*get home page for annie */
+module.exports.annieController = function (req, res, next) {
+    res.render('anniepage', {});
+};
+/*get home page for steph */
+module.exports.stephController = function (req, res, next) {
+    res.render('stephpage', {});
+};
+/*get home page for luiz */
+module.exports.luizController = function (req, res, next) {
+    res.render('luizpage', {});
+};
+/*get home page for valerie */
+module.exports.valerieController = function (req, res, next) {
+    res.render('valeriepage', {});
+};
+/*get home page for jambles */
+module.exports.jamblesController = function (req, res, next) {
+    res.render('jamblespage', {});
 };
--- app_server/routes/three.js
+++ app_server/routes/three.js
@@ -6,5 +6,12 @@ var ctrlS3 = require('../controllers/sample_3Controllers/test');
 /* base URL is '/samplethree' */
 router.get('/', ctrlS3.sampleHomeController);
 router.get('/ladypage', ctrlS3.ladyController);
+router.get('/daewoopage', ctrlS3.daewooController);
+router.get('/anniepage', ctrlS3.annieController);
+router.get('/stephpage', ctrlS3.stephController);
+router.get('/luizpage', ctrlS3.luizController);
+router.get('/valeriepage', ctrlS3.valerieController);
+router.get('/jamblespage', ctrlS3.jamblesController);
+
 
 module.exports = router;
