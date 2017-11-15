bab5edc515a002f7359a11040cb55d1010b9435a
mr.debbie <giyoungchong1980@gmail.com>
2017-11-15 07:50:17 -0500

    css fuss

--- public/javascripts/psychicc.js
+++ public/javascripts/psychicc.js
@@ -7,7 +7,7 @@ e.innerHTML = d;
 var userSign = document.getElementById("star");
 var forecast = document.getElementById("future");
 var fCast = function (){
-  if (userSign.value === "virgo" || userSign.value === "Virgo"){
+  /*if (userSign.value === "virgo" || userSign.value === "Virgo"){
     forecast.innerHTML = "Yo, Virgo! Here are your lucky numbers: 1,2,3";
   } else if (userSign.value === "aries" || userSign.value === "Aries"){
     forecast.textContent = "Aries, don't play lotto! Save your $";
@@ -34,6 +34,58 @@ var fCast = function (){
   }
    else {
     forecast.textContent = "You are out of this world!";
-  }
+  }*/
+  switch (userSign.value){
+  case 'virgo':
+  case 'Virgo':
+  forecast.textContent = "Yo, Virgo! Here are your lucky numbers: 1,2,3";
+  break;
+  case 'leo':
+  case 'Leo':
+  forecast.textContent = "Leo, take a nap and stay hydrated";
+  break;
+  case 'aries':
+  case 'Aries':
+  forecast.textContent = "Aries, don't play lotto! Save your $";
+  break;
+  case 'taurus':
+  case 'Taurus':
+  forecast.textContent = "Taurus, I like my steak medium rare";
+  break;
+  case 'gemini':
+  case 'Gemini':
+  forecast.textContent = "Dear Gemini, let go of your secrets";
+  break;
+  case 'cancer':
+  case 'Cancer':
+  forecast.textContent = "Cancer, look around for opportunity";
+  break;
+  case 'scorpio':
+  case 'Scorpio':
+  forecast.textContent = "Scorpio, please chill.";
+  break;
+  case 'sagittarius':
+  case 'Sagittarius':
+  forecast.textContent = "Greetings Sagittarius, remind yourself that you are worthy of love";
+  break;
+  case 'capricorn':
+  case 'Capricorn':
+  forecast.textContent = "Capricorn, live beyond this life.";
+  break;
+  case 'aquarius':
+  case 'Aquarius':
+  forecast.textContent = "Aquarius, fun and easy is best";
+  break;
+  case 'libra':
+  case 'Libra':
+  forecast.textContent = "Libra, your journey is not over";
+  break;
+  case 'pisces':
+  case 'Pisces':
+  forecast.textContent = "Pisces, don't eat that brownie.";
+  break;
+  default:
+  forecast.textContent = "You are out of this world!";
+}
 }
 userSign.addEventListener("input", fCast);
