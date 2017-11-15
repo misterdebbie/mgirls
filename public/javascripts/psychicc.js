b30cc2aaae824fa50820a3d5c869626a684edbfc
mr.debbie <giyoungchong1980@gmail.com>
2017-11-14 19:35:23 -0500

    update forecast

--- public/javascripts/psychicc.js
+++ public/javascripts/psychicc.js
@@ -12,25 +12,25 @@ var fCast = function (){
   } else if (userSign.value === "aries" || userSign.value === "Aries"){
     forecast.textContent = "Aries, don't play lotto! Save your $";
   } else if (userSign.value === "taurus" || userSign.value === "Taurus"){
-    forecast.textContent = "Taurus test";
+    forecast.textContent = "Taurus, call your mother!";
   } else if (userSign.value === "gemini" || userSign.value === "Gemini"){
-    forecast.textContent = "Gemini test";
+    forecast.textContent = "Dear Gemini, let go of your secrets.";
   } else if (userSign.value === "cancer" || userSign.value === "Cancer"){
     forecast.textContent = "Cancer test";
   } else if (userSign.value === "leo" || userSign.value === "Leo"){
     forecast.innerHTML = "Leo, take a nap and stay hydrated.";
   } else if (userSign.value === "scorpio" || userSign.value === "Scorpio"){
-    forecast.textContent = "Scorpio, please chill";
+    forecast.textContent = "Scorpio, please chill.";
   } else if (userSign.value === "sagittarius" || userSign.value === "Sagittarius"){
-    forecast.textContent = "Sagittarius test";
+    forecast.textContent = "Greetings Sagittarius, remind yourself that you are worthy of love.";
   } else if (userSign.value === "capricorn" || userSign.value === "Capricorn"){
-    forecast.textContent = "Capricorn test";
+    forecast.textContent = "Capricorn, live beyond this life.";
   } else if (userSign.value === "aquarius" || userSign.value === "Aquarius"){
-    forecast.textContent = "Aquarius test";
+    forecast.textContent = "Aquarius, fun and easy is best.";
   } else if (userSign.value === "libra" || userSign.value === "Libra"){
-    forecast.textContent = "Libra test";
+    forecast.textContent = "Libra, your journey is never over.";
   }else if (userSign.value === "pisces" || userSign.value === "Pisces"){
-    forecast.textContent = "Pisces test";
+    forecast.textContent = "Pisces, don't eat that brownie.";
   }
    else {
     forecast.textContent = "You are out of this world!";
